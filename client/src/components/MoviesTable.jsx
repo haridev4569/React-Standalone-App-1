8c7025a6cede4d4c1433d72e4c5381ba0d212617
haridev <haridevkambam@gmail.com>
2025-05-17 01:58:37 +0530

    add tanstack table

--- client/src/components/MoviesTable.jsx
+++ client/src/components/MoviesTable.jsx
@@ -54,17 +54,6 @@ const MoviesTable = () => {
     getSortedRowModel: getSortedRowModel(),
   });
 
-  const tableData = useMemo(() => {
-    return movies.map((movie) => (
-      <tr key={movie.imdbID}>
-        <td>{movie.Title}</td>
-        <td>{movie.Year}</td>
-        <td>{movie.Type}</td>
-        <td><img src={movie.Poster} alt={movie.Title} style={{ width: '50px', height: '50px' }} /></td>
-      </tr>
-    ))
-  }, [movies]);
-
   if (isLoading) return <div>Loading...</div>;
   if (!movies || movies.length === 0) return <p>Search for movies</p>;
 
